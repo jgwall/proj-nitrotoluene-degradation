2a29a37f322e345c791151aa5fbe877edfebcb41
Jason G. Wallace <jason.wallace@uga.edu>
2025-02-24 17:05:24 -0500

    Mon Feb 24 05:05:24 PM EST 2025 Make maize PCs

--- 0_InvestigateNitrotoluene.sh
+++ 0_InvestigateNitrotoluene.sh
@@ -2,11 +2,22 @@
 
 # Investigate nitrotoluene result from 2018 Phyllosphere GWAS
 
+# Allow calling conda
+. $(conda info --root)/etc/profile.d/conda.sh 
 
-# Correlation between Day and Night, split by sampling date (do both raw and log transformed, since mapped with log)
-#    Also figures out which varieties had most/least amounts
-Rscript 1a_CheckCorrelationDayNight.r --countfile PhyllosphereGwasData/4j_predicted_metagenome.ko.l3.biom.txt \
-  --keyfile PhyllosphereGwasData/2a_qiime_sample_key.tsv_corrected.txt -o 1a_nitrotoluene_correlations
+
+# # Correlation between Day and Night, split by sampling date (do both raw and log transformed, since mapped with log)
+# #    Also figures out which varieties had most/least amounts
+# Rscript 1a_CheckCorrelationDayNight.r --countfile PhyllosphereGwasData/4j_predicted_metagenome.ko.l3.biom.txt \
+#   --keyfile PhyllosphereGwasData/2a_qiime_sample_key.tsv_corrected.txt -o 1a_nitrotoluene_correlations
 
 
 # Determine the ultimate source of the nitrotoluene signal (=which organisms contributing to it)
+
+## PCA of genotypes and nitrotoluene signal
+conda activate tassel-5.2.89  # Tassel v5.2.89
+run_pipeline.pl -h PhyllosphereGwasData/0h_samples_sorted_filtered.hmp.txt.gz -distanceMatrix -MultiDimensionalScalingPlugin -endPlugin -export 1b_maize_pca.txt,1b_maize_pca.eigenvalues.txt 
+
+# OKay, PCAs made, next is to merge with data and plot
+
+# TODO
